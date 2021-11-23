51f5f24e4279890ede38f6e95b3accd3d2d4d693
srizzuto <srizzuto@gmail.com>
2021-11-23 00:41:34 -0300

    add sign in feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find(id: session[:user_id])
+    end
+  end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,7 @@
 class MainController < ApplicationController
 
   def index
-    if session[:user_id]
-      @user = User.find(id: session[:user_id])
-    end
+
   end
   
 end
