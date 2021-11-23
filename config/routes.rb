c999fb09445217230d89ba01b51b0a900bc3ae73
srizzuto <srizzuto@gmail.com>
2021-11-23 16:14:09 -0300

    add forgot your password feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "password", to: "passwords#edit", as: :edit_password
   patch "password", to: "passwords#update"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
