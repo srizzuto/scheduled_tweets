1dfd1826bb0c0112c4d304b6f25deb292cef0411
srizzuto <srizzuto@gmail.com>
2021-11-23 15:01:14 -0300

    add current user access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  before_action :set_current_user
+  
   def set_current_user
     if session[:user_id]
       Current.user = User.find(id: session[:user_id])
