e554f94305a37b944cef3058b58b3ec735df2483
srizzuto <srizzuto@gmail.com>
2021-12-08 14:57:07 -0300

    create twitter_account model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_many :twitter_accounts
+  
   has_secure_password
 
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email adress"}
